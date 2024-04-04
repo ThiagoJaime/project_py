08b091091850f3f450b0693dda8747b37c96134c
thiagoJaime <thiagojaimebarb@gmail.com>
2024-04-04 20:24:52 -0300

    start commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import re
+
+db = [
+   ['Candidato 1', 'e5_t10_p8_s8'],
+   ['Candidato 2', 'e10_t7_p7_s8'],
+   ['Candidato 3', 'e8_t5_p4_s9'],
+   ['Candidato 4', 'e2_t2_p2_s1'],
+   ['Candidato 5', 'e10_t10_p8_s9']
+]
+
+if __name__ == "__main__":
+    main()

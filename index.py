bf679b805d9b0d8b4ed940ad8fe65b8418ddb841
thiagoJaime <thiagojaimebarb@gmail.com>
2024-04-05 20:16:08 -0300

    last details

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+#importação do módulo 're' para simplificar as consultas
 import re
 
 db = [
@@ -8,18 +9,22 @@ db = [
    ['Candidato 5', 'e10_t10_p8_s9']
 ]
 
+#função para validação do padrão definido
 def validaInput(entrada):
     padrao = r'^\d+,\d+,\d+,\d+$'
     return bool(re.match(padrao, entrada))
 
+#função para pegar tudo entre (0-9) na string
 def extrairNumbers(notas):
     numeros = []
     for nota in notas:
+        #usando método extend junto do list comprehension + findall para pegar as notas em int
         numeros.extend([int(num) for num in re.findall(r'\d+', nota)])
     return numeros
    
 def main():
 
+   #vai repetir até o usuário colocar no formato correto
    while True:
       notas = input('''
          Digite as notas: 1º entrevista, 2º teórico, 3º prático, 4º softSkill
@@ -31,47 +36,44 @@ def main():
       else:
          break
    
-   listaNota = notas.split(',')
+   #lista em int do input do user
+   listaNota = list(map(int, notas.split(',')))
+
    listaPesquisa = []
-   MAX_SIZE = len(db)
-   cont = 0
 
-   while True:
+   #looping para ir em cada candidato
+   for candidato in db:
 
-      for candidato in db:
+      #pega as notas de cada usuário
+      numbers = extrairNumbers(candidato[1].split('_'))
 
-         numbers = extrairNumbers(candidato[1].split('_'))
+      ehMenor = False
 
-         ehMenor = False
+      #compara as notas em cada lista
+      for i in range(len(numbers)):
 
-         for i in range(len(numbers)):
-
-               if(numbers[i] < int(listaNota[i])):
-                  ehMenor = True
-                  break
-         
-         if(not ehMenor):
-               
-               listaPesquisa.append(candidato)   
+            if(numbers[i] < listaNota[i]):
+               ehMenor = True
+               break
       
-         cont += 1
-
-      if(cont == MAX_SIZE):
-         
-         if(not listaPesquisa):
-                  
-               print("Resultados não compatíveis")
-                  
-         else:
-                  
-               for candidato in listaPesquisa:
-                  
-                  user = extrairNumbers(candidato[1].split('_'))
-
-                  print(f'''{candidato[0]}: Entrevista: {user[0]}, Teórico: {user[1]}, Prático: {user[2]}, SoftSkill: {user[3]}
---------------------------------------------------------------------''') 
+      if(not ehMenor):
             
-         break
-      
+            listaPesquisa.append(candidato)   
+
+   #se a lista estiver vazia
+   if(not listaPesquisa):
+            
+      print("Resultados não compatíveis")
+            
+   else:
+
+      #exibe os resultados      
+      for candidato in listaPesquisa:
+         
+         user = extrairNumbers(candidato[1].split('_'))
+
+         print(f'''{candidato[0]}: Entrevista: {user[0]}, Teórico: {user[1]}, Prático: {user[2]}, SoftSkill: {user[3]}
+# ----------------------------------------------------------------- #''') 
+                 
 if __name__ == "__main__":
     main()
